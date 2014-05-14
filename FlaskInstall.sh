73de376bec67b07f22ca66be46f5eed0d02a4f18
vrk7bp <vrk7bp@virginia.edu>
2014-05-14 14:15:35 -0400

    Trying to get Flask to work

---
+++ FlaskInstall.sh
@@ -0,0 +1,12 @@
+wget http://github.com/mitsuhiko/flask/zipball/0.6
+unzip mitsuhiko-flask-0.6-0-g5cadd9d.zip
+cp -r mitsuhiko-flask-5cadd9d/flask ~/GautamWebsite/
+wget http://pypi.python.org/packages/source/W/Werkzeug/Werkzeug-0.6.2.tar.gz
+tar -xvzf Werkzeug-0.6.2.tar.gz
+cp -r Werkzeug-0.6.2/werkzeug ~/GautamWebsite/
+wget http://pypi.python.org/packages/source/J/Jinja2/Jinja2-2.5.tar.gz
+tar -xvzf Jinja2-2.5.tar.gz
+cp -r Jinja2-2.5/jinja2 ~/GautamWebsite/
+wget http://pypi.python.org/packages/source/s/simplejson/simplejson-2.1.1.tar.gz
+tar -xvzf simplejson-2.1.1.tar.gz
+cp -r simplejson-2.1.1/simplejson ~/GautamWebsite/
