883f5317a4691144c80eb60a7e043bf781cf2eb3
vrk7bp <vrk7bp@virginia.edu>
2014-05-15 19:51:54 -0400

    Adding werkzeug, simplejson, and jinja.

---
+++ FlaskServer.sh
@@ -0,0 +1,9 @@
+wget http://pypi.python.org/packages/source/W/Werkzeug/Werkzeug-0.6.2.tar.gz
+tar -xvzf Werkzeug-0.6.2.tar.gz
+cp -r Werkzeug-0.6.2/werkzeug ~/GautamWebsite/
+wget http://pypi.python.org/packages/source/J/Jinja2/Jinja2-2.5.tar.gz
+tar -xvzf Jinja2-2.5.tar.gz
+cp -r Jinja2-2.5/jinja2 ~/GautamWebsite/
+wget http://pypi.python.org/packages/source/s/simplejson/simplejson-2.1.1.tar.gz
+tar -xvzf simplejson-2.1.1.tar.gz
+cp -r simplejson-2.1.1/simplejson ~/GautamWebsite/
--- FlaskServer.wsgi
+++
@@ -1,4 +0,0 @@
-import sys
-sys.path.insert(0, '/var/www/GautamWebsite')
-
-from FlaskServer import FlaskServer as application
