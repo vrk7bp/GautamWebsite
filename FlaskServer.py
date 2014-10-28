8a32da30ec58b011c8e97cb990f8282b3bb26806
vrk7bp <vrk7bp@virginia.edu>
2014-10-27 23:45:16 -0400

    Testing for Chad

--- FlaskServer.py
+++ FlaskServer.py
@@ -13,6 +13,11 @@ def hello():
 def aboutMe():
 	return render_template('AboutMe.html')
 
+@app.route('/chad')
+def chad(): {
+	return "hello chad"
+}
+
 @app.route('/testing')
 def testing():
 	return render_template('MainPageSeptember6th2014.html')
